d707f8a39c452eb51f9499711297a97c207f1694
Benjamin Groß <ben@benbox.org>
2021-11-23 23:50:16 +0100

    remarks

--- data-raw/ourdata.R
+++ data-raw/ourdata.R
@@ -6,6 +6,8 @@ library(readxl)
 
 imr <- read_csv("data-raw/IMR.csv")
 hdi <- read_csv("data-raw/HDI.csv")
+koelsch <- read_csv("data-raw/Koelsch.csv")
+kirche <- read_csv("data-raw/Kirche.csv")
 
 # Data cleaning code here...
 # (Do NOT put data analysis code here!)
@@ -14,4 +16,4 @@ hdi <- read_csv("data-raw/HDI.csv")
 # Note that names are unquoted.
 # I like using overwrite = T so everytime I run the script the
 # updated objects are saved, but the default is overwrite = F
-usethis::use_data(imr, hdi, overwrite = TRUE)
+usethis::use_data(imr, hdi, koelsch, kirche, overwrite = TRUE)
