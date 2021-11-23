5304ae16ec9e952fd28351c0ec334bda2abf0bb3
Benjamin Groß <ben@benbox.org>
2021-11-24 00:46:43 +0100

    remarks

--- R/hello.R
+++ R/hello.R
@@ -15,4 +15,7 @@
 
 hello <- function() {
   print("Dies ist das R-Paket für den Kurs Technical Applications & Data Analytics!")
+  print("help(ourdata) um den Inhalt des Pakets anzuzeigen.")
+  print("Viel Spass im Kurs!")
+  print("Ben")
 }
