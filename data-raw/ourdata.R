92e5cadd7f687fcac8e04577dc269e424ea59f5f
Benjamin Groß <ben@benbox.org>
2021-11-23 23:26:13 +0100

    remarks

---
+++ data-raw/ourdata.R
@@ -0,0 +1,17 @@
+# data-raw/ourdata.R
+# Data import and processing pipeline
+
+library(readr)
+library(readxl)
+
+imr <- read_csv("data-raw/IMR.csv")
+hdi <- read_csv("data-raw/HDI.csv")
+
+# Data cleaning code here...
+# (Do NOT put data analysis code here!)
+
+# This should be the last line.
+# Note that names are unquoted.
+# I like using overwrite = T so everytime I run the script the
+# updated objects are saved, but the default is overwrite = F
+usethis::use_data(imr, hdi, overwrite = TRUE)
