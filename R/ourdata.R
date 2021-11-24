a414c6b67e7ea37e2f21b9001eb38edcdad5f36b
Benjamin Groß <ben@benbox.org>
2021-11-24 11:05:25 +0100

    remarks

---
+++ R/ourdata.R
@@ -0,0 +1,8 @@
+# Hello, Students!
+
+ourdata <- function() {
+  print("Dies ist das R-Paket ourdata für den Kurs Technical Applications & Data Analytics!")
+  print("help(ourdata) um den Inhalt des Pakets anzuzeigen.")
+  print("Viel Spass im Kurs!")
+  print("Ben")
+}
