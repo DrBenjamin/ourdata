695f89121fd985559dbf8445424d90da943f5bd3
Benjamin Groß <ben@benbox.org>
2021-11-24 10:58:19 +0100

    remarks

--- data-raw/ourdata.R
+++ data-raw/ourdata.R
@@ -8,7 +8,7 @@ imr <- read_csv("data-raw/IMR.csv")
 hdi <- read_csv("data-raw/HDI.csv")
 koelsch <- read_csv("data-raw/Koelsch.csv")
 kirche <- read_csv("data-raw/Kirche.csv")
-kopfumfang <- read_csv("data-raw/Kopfumfang.csv")
+fragebogen <- read_csv("data-raw/Fragebogen.csv")
 
 # Data cleaning code here...
 # (Do NOT put data analysis code here!)
@@ -17,4 +17,4 @@ kopfumfang <- read_csv("data-raw/Kopfumfang.csv")
 # Note that names are unquoted.
 # I like using overwrite = T so everytime I run the script the
 # updated objects are saved, but the default is overwrite = F
-usethis::use_data(imr, hdi, koelsch, kirche, kopfumfang, overwrite = TRUE)
+usethis::use_data(imr, hdi, koelsch, kirche, fragebogen, overwrite = TRUE)
