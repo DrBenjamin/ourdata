ae024820348c3b762f23f35fd5c5966466dae2a4
Benjamin Groß <ben@benbox.org>
2021-11-25 16:04:15 +0100

    edited text

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("This is version ", packageVersion(pkgname),
-                        " of ", pkgname, "\nType help(ourdata)")
+                        " of ", pkgname, "\nType `help(ourdata)´")
 }
 
 .onLoad <- function(libname, pkgname) {
