22bd1c5b5e81db349f8cf279a9a89a8378a972db
Benjamin Groß <ben@benbox.org>
2021-11-26 21:57:00 +0100

    changed str of list1 & list2 from char to num

--- R/combine.R
+++ R/combine.R
@@ -4,20 +4,17 @@
 combine <- function(id, foreign_key, list1, list2, col1 = "C1", col2 = "C2", col3 = "C3") {
   # Two For-Loops for matchting two lists
   com_data <- data.frame(col1 = character(),
-                         col2 = character(),
-                         col3 = character(),
+                         col2 = numeric(),
+                         col3 = numeric(),
                          stringsAsFactors = FALSE)
-  print(str(com_data))
   for (i in 1:length(id)) {
-    #print(paste0("Left list: ", id[i], " | position ", i))
     for (ii in 1:length(foreign_key)) {
       if(id[i] == foreign_key[ii]) {
-        #print(paste0("Right list: ", freign_key[ii], " | position = ", ii))
-        com_data <- rbind(com_data, c(id[i], list1[i], list2[ii]))
+        com_data <- rbind.data.frame(com_data, data.frame(as.character(id[i]), as.numeric(list1[i]), as.numeric(list2[ii]), stringsAsFactors = FALSE), stringsAsFactors = FALSE)
       }
     }
   }
   colnames(com_data) <- c(col1, col2, col3)
-  print(str(com_data))
+  str(com_data)
   return(com_data)
 }
