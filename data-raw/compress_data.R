60dc3920e1026b6a95e3558362a9bfa7103fe290
Benjamin Groß <ben@benbox.org>
2021-11-26 22:29:43 +0100

    added compression skript

---
+++ data-raw/compress_data.R
@@ -0,0 +1,9 @@
+# data-raw/compress_data.R
+# compress all data files (Rdata)
+
+save(fragebogen, file = "data/fragebogen.rda", compress = "bzip2")
+save(hdi, file = "data/hdi.rda", compress = "xz")
+save(imr, file = "data/imr.rda", compress = "xz")
+save(kirche, file = "data/kirche.rda", compress = "bzip2")
+save(koelsch, file = "data/koelsch.rda", compress = "bzip2")
+
