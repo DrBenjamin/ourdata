bf312d8a0778571410627f5598c25558ed613651
Benjamin Groß <ben@benbox.org>
2021-11-28 18:28:09 +0100

    added flexible routine to detect unique values

--- R/transformer.R
+++ R/transformer.R
@@ -4,24 +4,33 @@
 
 transformer <- function(x, verbose = FALSE) {
   transformed_data <- data.frame(col = numeric(), stringsAsFactors = FALSE)
-  print(sapply(x, class))
   x <- as.vector(unlist(x))
-  print(sapply(x, class))
-  i <- 0
-  ii <- 0
+
+  # Search for different values which should be transformed
+  counterValues <- 1
+  charValues <- c("", "")
+  for (c in 1:length(x)) {
+    for (cc in 1:counterValues)
+      if(x[c] != charValues[cc]) {
+        charValues[counterValues] = x[c]
+      }
+    counterValues <- length(charValues)
+  }
+
+  counterUniques <- c(as.numeric(0), as.numeric(0))
   for (vItem in x) {
-    if(vItem == "female") {
-      i <- i + 1
-      transformed_data <- rbind.data.frame(transformed_data, as.numeric("1"), stringsAsFactors = FALSE)
-    }
-    if(vItem == "male") {
-      ii <- ii + 1
-      transformed_data <- rbind.data.frame(transformed_data, as.numeric("2"), stringsAsFactors = FALSE)
+    for (c in 1:counterValues) {
+      if(vItem == charValues[c]) {
+        counterUniques[c] <- counterUniques[c] + 1
+        transformed_data <- rbind.data.frame(transformed_data, as.numeric(c), stringsAsFactors = FALSE)
+      }
     }
   }
   if(verbose == TRUE) {
-    print(paste0("female: ", i))
-    print(paste0("male: ", ii))
+    print(paste0(length(x), " values in list."))
+    print(paste0("Count of unique values: ", counterValues))
+    for(c in 1:counterValues)
+      print(paste0("Unique vaulue ", charValues[c]," has ", counterUniques[c]), " hits.")
   }
   return(transformed_data)
 }
