673cb5a33ce52888525f33a403f251ffa0ddd889
Benjamin Groß <ben@benbox.org>
2021-11-30 18:55:17 +0100

    added menu and 3 type of plots

--- R/plotter.R
+++ R/plotter.R
@@ -2,19 +2,89 @@
 # function plotter' for plotting data with a menu to choose different plotting types
 # e.g. 'plot' for a Scatter plot
 
-plotter <- function(x, y = "None", verbose = FALSE) {
-  x_name <- names(x)
-  x <- as.vector(unlist(x))
-  y_name <- names(y)
-  y <- as.vector(unlist(y))
+plotter <- function(x = NULL, y = NULL, plot_type = NULL, language = NULL, verbose = FALSE) {
 
-  # plotting
-  if(y == 'None') barplot(x)
-  if(y != 'None') plot(x ~ y)
+  # User input
+  # Language
+  if(is.null(language)) {
+    user_input1 <- readline(prompt = "Language ('ger' or 'eng')? ")
+  } else { user_input1 = language }
 
-  # verbose messages
-  if(verbose == TRUE) {
-    print("plot(x ~ y)")
+  # checking if x data is available
+  if(is.null(x)) {
+    user_inputx <- ""
+    while (user_inputx == "") {
+      if(user_input1 == 'eng') {
+        # English
+        user_inputx <- readline(prompt = "Name of the data frame or vector? ")
+      } else {
+        # German
+        user_inputx <- readline(prompt = "Name des Data Frames oder Vektors? ")
+      }
+    }
+    # problems with 'data_frame$column' writing in a variable with name of vector, solution ->
+    if(grepl("$", user_inputx, fixed = TRUE)) {
+      # data frame column to vector (x)
+      x <- as.vector(unlist(eval(parse(text=paste0(user_inputx)))))
+    } else {
+      if(is.data.frame(get(user_inputx))) {
+        # read a data frame from input from console
+        x <- get(user_inputx)
+        x <- as.vector(unlist(x))
+      } else {
+        # read a vector from input from console
+        x <- get(user_inputx)
+      }
+    }
+  } else {
+      if(is.data.frame(x)) {
+        # got a data frame as 'x' in function load
+        x <- as.vector(unlist(x))
+      } else {
+        # got a vector as 'x' in function load
+      }
   }
-  return(print("Done."))
+
+  # checking if plot_type is not choosen
+  if(is.null(plot_type)) {
+    # checking if y data is available or not
+    if(is.null(y)) {
+      if(user_input1 == 'eng') {
+        # English
+        user_input2 <- readline(prompt = "(B)arplot or (D)ensity Plot? ")
+      } else {
+        # German
+        user_input2 <- readline(prompt = "(B)alkendiagramm oder (D)ichtediagramm? ")
+      }
+    } else {
+      y <- as.vector(unlist(y))
+      if(user_input1 == 'eng') {
+        # English
+        user_input2 <- readline(prompt = "(S)catterplot? ")
+      } else {
+        # German
+        user_input2 <- readline(prompt = "(S)treudiagramm? ")
+      }
+    }
+    plot_type <- user_input2
+  }
+
+  # do the plotting
+  if(plot_type == 'S' | plot_type == 's') {
+    plot(x, y)
+  }
+  if(plot_type == 'B' | plot_type == 'b') {
+    barplot(x)
+  }
+  if(plot_type == 'D' | plot_type == 'd') {
+    plot(density(x))
+  }
+
+  if(verbose == TRUE) {
+    if(user_input1 == 'eng') {
+      return(print("Done."))
+    } else {
+      return(print("Erledigt."))
+    }
+  }
 }
