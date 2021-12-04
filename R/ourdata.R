fb8dc073376a881fc19c9ce3a703045c949134cd
Benjamin Groß <ben@benbox.org>
2021-12-04 10:52:01 +0100

    used 'cat' instead of 'print'

--- R/ourdata.R
+++ R/ourdata.R
@@ -2,8 +2,8 @@
 # Startup message
 
 ourdata <- function() {
-  print("This is the R package ourdata used in the Technical Applications & Data Analytics course!")
-  print("Type `help(ourdata)´ to display the content.")
-  print("Have fun in the course!")
-  print("Ben")
+  cat("This is the R package ourdata used in the Technical Applications & Data Analytics course!")
+  cat("Type `help(ourdata)´ to display the content.")
+  cat("Have fun in the course!")
+  cat("Ben")
 }
