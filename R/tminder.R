a1b0c5a4942459c65686ee95da3b4bd68384e907
Benjamin Groß <ben@benbox.org>
2021-12-05 12:29:25 +0100

    change file location

--- R/tminder.R
+++ R/tminder.R
@@ -5,7 +5,7 @@ tminer <- function(x, lang)
 {
     library(reticulate)
     use_virtualenv("my_env")
-    source_python("./py/py_deepl.py")
+    source_python("py_deepl.py")
 
     return(py_deepl(x, lang))
 }
