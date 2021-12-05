e0944761677e9400a7e01eb898c8e2519d25f019
Benjamin Groß <ben@benbox.org>
2021-12-05 15:29:35 +0100

    corrected file path to py_deepl.py

--- R/tminder.R
+++ R/tminder.R
@@ -5,7 +5,7 @@ tminer <- function(x, lang)
 {
     library(reticulate)
     use_virtualenv("inst/extdata/my_env")
-    source_python("py_deepl.py")
+    source_python(system.file("extdata", "py_deepl.py", package = "ourdata"))
 
     return(py_deepl(x, lang))
 }
