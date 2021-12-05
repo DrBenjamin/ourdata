f055f9ab9159e6f9a90fe6f13cfe3787413c3200
Benjamin Groß <ben@benbox.org>
2021-12-05 23:50:40 +0100

    added 'example.R' file for showing external executing in R-Markdown

---
+++ inst/extdata/example.R
@@ -0,0 +1,15 @@
+## inst/extdata/example.R
+## Beispiel Code zum veranschaulichen der externen Code-Ausführung in R Markdown
+
+## @knitr variablesXY
+
+# x Werte von 1 und 100 zuweisen
+x <- 1:100
+# y 100 Werte zuweisen mit x + Zufallszahl (-5 bis +5)
+y <- x + rnorm(100, sd = 5)
+# Gibt den ersten oder letzten Teil eines Vektors, einer Matrix oder einer Tabelle zurück
+head(data.frame(x, y))
+
+## @knitr plotXY
+
+plot(x, y)
