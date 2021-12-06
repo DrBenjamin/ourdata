01ca1e8bd0572a7bcd6230dace8892908f83d35c
Benjamin Groß <ben@benbox.org>
2021-12-06 22:11:55 +0100

    minor comment fixes

--- inst/extdata/py_deepl.py
+++ inst/extdata/py_deepl.py
@@ -1,10 +1,13 @@
-## ./py/py_deepl.py
-## python script for using deepl api for translating
+## inst/extdata/py_deepl.py
+## python script for using DeepL API for translating
 
+# Python Bibliothek 'DeepL' laden
 import deepl
 
 def py_deepl(x, lang):
+  # Übersetzung mit DeepL API (Python)
   translator = deepl.Translator("c52a9c7d-3198-063c-2bbf-8f67173820ce:fx")
   result = translator.translate_text(x, target_lang = lang) 
-
+  
+  # Rückgabe der Daten
   return result
