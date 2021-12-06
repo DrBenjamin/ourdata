415c82ce597e67e1dfaf4f2ca415e28d5b5d6620
Benjamin Groß <ben@benbox.org>
2021-12-06 20:24:34 +0100

    deleted shiny support

--- R/zzz.R
+++ R/zzz.R
@@ -8,7 +8,11 @@
 
 .onLoad <- function(libname, pkgname) {
   ourdata()
-  #url <- as.character(system.file("rmd", "ourdata.html", package = "ourdata", mustWork = FALSE))
-  #browseURL(url, browser = getOption("browser"), encodeIfNeeded = FALSE)
-  rmarkdown::run(system.file("rmd", "ourdata.Rmd", package = "ourdata", mustWork = FALSE))
+
+  # Loading the HTML 'ourdata' Page
+  url <- as.character(system.file("rmd", "ourdata.html", package = "ourdata", mustWork = FALSE))
+  browseURL(url, browser = getOption("browser"), encodeIfNeeded = FALSE)
+
+  # Running the Rmd file directly
+  #rmarkdown::run(system.file("rmd", "ourdata.Rmd", package = "ourdata", mustWork = FALSE))
 }
